d5a41a573a893bb906c39760f378fde35dce1a6f
Robert Ponikiewski <dominik.z.2020@gmail.com>
2024-02-17 23:55:55 +0100

    Create appdelegate2.hpp

---
+++ appdelegate2.hpp
@@ -0,0 +1,13 @@
+//
+//  appdelegate2.hpp
+//  render
+//
+//  Created by MotionVFX on 17/02/2024.
+//
+
+#ifndef appdelegate2_hpp
+#define appdelegate2_hpp
+
+#include <stdio.h>
+
+#endif /* appdelegate2_hpp */
